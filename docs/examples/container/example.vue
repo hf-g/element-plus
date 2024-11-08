7058c8b14d4fcffe1d21e0633eb87706feefc141
hf-g <2149759091@qq.com>
2024-11-08 14:40:34 +0800

    Update example.vue

--- docs/examples/container/example.vue
+++ docs/examples/container/example.vue
@@ -9,8 +9,8 @@
             </template>
             <el-menu-item-group>
               <template #title>Group 1</template>
-              <el-menu-item index="1-1">Option 1</el-menu-item>
-              <el-menu-item index="1-2">Option 2</el-menu-item>
+              <el-menu-item index="1-1">h</el-menu-item>
+              <el-menu-item index="1-2">f</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="Group 2">
               <el-menu-item index="1-3">Option 3</el-menu-item>
